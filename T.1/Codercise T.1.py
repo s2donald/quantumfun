6eac8ea7bb7cedd463de6ffd7bd0a2587740cf29
s2donald <s2donald@uwaterloo.ca>
2023-12-09 04:47:11 -0500

    Initializing Repository to show quantum tutorial

---
+++ T.1/Codercise T.1.py
@@ -0,0 +1,15 @@
+def add_numbers(x, y):
+    """A simple function to add two numbers.
+
+    Args:
+        x (float): a number
+        y (float): another number
+
+    Returns:
+        float: The sum of x and y.
+    """
+
+    ##################
+    # YOUR CODE HERE #
+    ##################
+    return x + y
---
+++ T.1/Codercise T.2.py
@@ -0,0 +1,15 @@
+import numpy as np
+def find_max(x):
+    """A simple function to find the largest value in a numpy array.
+
+    Args:
+        x (array[float]): an array of numbers of size n
+
+    Returns:
+        float: The maximum value in x.
+    """
+
+    ##################
+    # YOUR CODE HERE #
+    ##################
+    return np.max(x)
